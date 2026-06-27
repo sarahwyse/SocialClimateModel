e0096c2d27742d8d69076af5b5b781cf82fc27c5
sarahwyse <73261407+sarahwyse@users.noreply.github.com>
2026-06-27 10:44:11 -0700

    Update fullModel_rep.py
    Added some line commenting.

--- fullModel_rep.py
+++ fullModel_rep.py
@@ -1,13 +1,12 @@
 #the full model using opinion response functions as the social model and the SDE energy balance as the climate model
 #this version saves data from repeated simulations to an csv file
 
+#to change simulations - number of simulations to run (line 66), memory bank size (line 31), file save locations (lines 105-108)
+
 import numpy as np
 import numpy.random as rnd
 from scipy.stats import binom
 import pandas as pd
-import time
-
-t1 = time.time()
 
 #time parameters
 yrToSec = 365*24*60*60   #original model was in seconds, use this as part of C to convert to years
@@ -29,11 +28,11 @@ theta = 1/5                 #5 year window for autocorrelation
 sigma = np.sqrt(dt/yrToSec) #magnitude of stochasticity (1 degree over 80 years)
 
 #social model parameters
-M = 25                      #memory bank length - we choose a fixed value here
-CM = np.zeros(int(years/dt))                    #committed minority
-CM[0] = 0.1
+M = 25                      #memory bank length - we choose a fixed value for our simulations
+CM = np.zeros(int(years/dt))        #to track committed minority proportion over the simulation
+CM[0] = 0.1                         #comitted minority initial condition
 r = CM[0]                       #speaking rate of opinion A is initially CM=0.1 (no one uncommitted holds opinion A)
-r_track = np.zeros(int(years/dt))  #to track the speaking rate of A over time without interferring with root finding
+r_track = np.zeros(int(years/dt))  #to track the speaking rate of A over time without interferring with root finding for solving social model
 r_track[0] = r
 error = 1e-6                #error bound in root finding search
 
@@ -63,49 +62,48 @@ for i in range(1,len(data.years)-1):
 emis_ppm_best = dt*emis_best/7.76 #convert from Gt to ppm CO2, multiply by dt to average emissions across the year
 emis_ppm_worst = dt*emis_worst/7.76
 
-repetitions=10
+#set up arrays to track data from multiple simulations
+repetitions=10   #number of simulations to run
 r_end = np.zeros(repetitions)
 CM_max = np.zeros(repetitions)
+
 #run simulation
 for rep in range(repetitions):
-    #set things to 0 as needed
-    r = CM[0]
+    r = CM[0] #start each simulation from the initial condition for climate action speaking rate
     for t in range(1, int(years/dt)):
         #get equilibrium value from social model by iterating until within error bound
-        r_new = -1  #place holder value so that we can go into the while loop
+        r_new = -1  #place holder value so that we can get into the while loop
         while abs(r-r_new)>error:   #iterate until we reach a fixed point
             r_temp = r
-            if r>1:         #so that numerical errors don't cause r_new=nan
+            if r>1:  #so that numerical errors don't cause r_new=nan
                 r=1
             if (M%2):
-                r_new = CM[t-1] + (1-CM[t-1])*(sum([binom.pmf(i,M,r) for i in range(int((M+1)/2), M+1)]))
+                r_new = CM[t-1] + (1-CM[t-1])*(sum([binom.pmf(i,M,r) for i in range(int((M+1)/2), M+1)])) #even number of memories, no undecided individuals
             else:
-                r_new = CM[t-1] + (1-CM[t-1])*(sum([binom.pmf(i,M,r) for i in range(int(M/2)+1, M+1)]) + 0.5*binom.pmf(int(M/2),M,r))
+                r_new = CM[t-1] + (1-CM[t-1])*(sum([binom.pmf(i,M,r) for i in range(int(M/2)+1, M+1)]) + 0.5*binom.pmf(int(M/2),M,r)) #odd number of memories, there exist undecided individuals
             r = r_new
             r_new = r_temp
         r_track[t] = r
         if r==1: 
             break 
-        #get emissions level to plug into climate model
+        #get emissions amount in ppm and add to previous CO2 concentration to get current CO2 concentration to plug into climate model
         emis_ppm_actual[t] = r*emis_ppm_best[t] + (1-r)*emis_ppm_worst[t]
         conc[t] = conc[t-1] + emis_ppm_actual[t]    
         #define a random number to add stochasticity
         rand = np.random.normal(0, 1, None)
-        #solve stoch via Euler Maruyama
+        #solve stochastic equation via Euler Maruyama
         W[t] = W[t-1] - theta*W[t-1]*dt + sigma*rand
         #solve climate model via Euler Maruyama
         T[t] = T[t-1] + dt*(S*(1-alpha)/4 - (eta+beta*T[t-1]) + a*np.log(conc[t-1]/pre_ind))/C + W[t]
-        #temperature impacts the social model
+        #compute temperature impact on the social model
         lam = dt*lambda_0*(1.1**(T[t]-T_PI))  #current rate of EE per timestep
         EE[t] = rnd.poisson(lam)                 #number of extreme events in the timestep
-        CM[t] = CM[0] + mu*sum(EE[i]*np.exp(-delta*(t-i)) for i in range(t+1))    
+        CM[t] = CM[0] + mu*sum(EE[i]*np.exp(-delta*(t-i)) for i in range(t+1))  #updated committed minority size   
     #save data, each new row is a new simulation
     r_end[rep] = r   
     CM_max[rep] = np.max(CM[0:t])
-    #pd.DataFrame(T).T.to_csv(r'/home/swyse/Documents/Graduate School/Thesis/Figures/SCM/data_temp2.csv', index=False, header=False, mode='a')    #temperature
-    #pd.DataFrame(r_track).T.to_csv(r'/home/swyse/Documents/Graduate School/Thesis/Figures/SCM/data_r2.csv', index=False, header=False, mode='a') #the speaking rate of opinion A over time
-    #pd.DataFrame(CM).T.to_csv(r'/home/swyse/Documents/Graduate School/Thesis/Figures/SCM/data_CM2.csv', index=False, header=False, mode='a')     #committed minority proportion (drives speaking rate of A)
-    #pd.DataFrame(EE).T.to_csv(r'/home/swyse/Documents/Graduate School/Thesis/Figures/SCM/data_EE2.csv', index=False, header=False, mode='a')     #extreme event occurance
+    #pd.DataFrame(T).T.to_csv(r'/FILE_DIRECTORY/data_temp.csv', index=False, header=False, mode='a')    #temperature
+    #pd.DataFrame(r_track).T.to_csv(r'/FILE_DIRECTORY/data_r.csv', index=False, header=False, mode='a') #speaking rate of opinion A over time
+    #pd.DataFrame(CM).T.to_csv(r'/FILE_DIRECTORY/data_CM.csv', index=False, header=False, mode='a')     #committed minority proportion (drives speaking rate of A)
+    #pd.DataFrame(EE).T.to_csv(r'/FILE_DIRECTORY/data_EE.csv', index=False, header=False, mode='a')     #extreme event occurance
         
-
-t2 = time.time() - t1
